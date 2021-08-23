9bab4d853f817736903643fd29f8536d8ab8bc37
William Mucheru <willyk99@gmail.com>
2021-08-23 18:51:18 +0300

    Cleanup, load nodes from json

--- demos/think/assets/js/main.js
+++ demos/think/assets/js/main.js
@@ -1,57 +1,23 @@
-var margin = {
-        top: 10,
-        right: 30, bottom: 30, left: 40
-    },
-    width = 600 - margin.left - margin.right,
-    height = 600 - margin.top - margin.bottom;
+$(document).ready(function () {
+    $.getJSON('assets/network.json', function (data) {
+        console.log(data);
 
-const svg = d3.select("#network-graph")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");;
+        var graph = new Springy.Graph(),
+            nodes = [];
 
-d3.json("assets/nodes-d3.json", function (data) {
-    console.log(data);
+        data.nodes.map(function (n, i) {
+            nodes.push(graph.newNode({ label: n.name }))
+        });
 
-    // Links
-    var link = svg
-        .selectAll("line")
-        .data(data.links)
-        .enter()
-        .append("line")
-        .style("stroke", "#222")
+        data.nodes.map(function (n, i) {
+            graph.newEdge(nodes[0], nodes[i], { color: n.color })
+        });
 
-    // Nodes
-    var node = svg
-        .selectAll("circle")
-        .data(data.nodes)
-        .enter()
-        .append("circle")
-        .attr("r", 20)
-        .style("fill", "#81509B")
-    
-    var simulation = d3.forceSimulation(data.nodes)                 // Force algorithm is applied to data.nodes
-        .force("link", d3.forceLink()                               // This force provides links between nodes
-            .id(function(d) { return d.id; })                     // This provide  the id of a node
-            .links(data.links)                                    // and this the list of links
-        )
-        .force("charge", d3.forceManyBody().strength(-400))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
-        .force("center", d3.forceCenter(width / 2, height / 2))     // This force attracts nodes to the center of the svg area
-        .on("end", ticked);
-
-    // This function is run at each iteration of the force algorithm, updating the nodes position.
-    function ticked() {
-        link
-            .attr("x1", function (d) { return d.source.x; })
-            .attr("y1", function (d) { return d.source.y; })
-            .attr("x2", function (d) { return d.target.x; })
-            .attr("y2", function (d) { return d.target.y; });
-
-        node
-            .attr("cx", function (d) { return d.x + 6; })
-            .attr("cy", function (d) { return d.y - 6; });
-    }
+        jQuery('#springy').springy({
+            graph: graph,
+            nodeSelected: function (node) {
+                console.log('Selected: ', node);
+            }
+        });
+    });
 });
